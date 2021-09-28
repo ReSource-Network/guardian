1f5cb9826d28d827f58b1acbdd28a27090bdb3b4
bridgerz <bridgerzoske@gmail.com>
2021-09-27 22:55:56 -0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,9 @@
 import dotenv from "dotenv";
-import { ethers } from "ethers";
-import * as fs from "fs";
-import * as path from "path";
 dotenv.config();
 
 export function isProd() {
   return process.env.NODE_ENV === "production";
 }
-
-const getPKFromMnemonic = (): string => {
-  const filePath = `./../mnemonic.txt`;
-  const mnemonic = fs
-    .readFileSync(path.join(__dirname, filePath))
-    .toString()
-    .trim();
-  return ethers.Wallet.fromMnemonic(mnemonic).privateKey;
-};
 
 export function fetchConfig() {
   return {
@@ -24,7 +12,7 @@ export function fetchConfig() {
     POSTGRES: process.env.POSTGRES!,
     JWT_SECRET: process.env.JWT_SECRET!,
     TOTP_SECRET: process.env.TOTP_SECRET!,
-    GUARDIAN_WALLET_PK: getPKFromMnemonic(),
+    GUARDIAN_WALLET_PK: process.env.GUARDIAN_WALLET_PK!,
     BLOCKCHAIN_NETWORK: process.env.BLOCKCHAIN_NETWORK!,
     SENTRY_DSN: process.env.SENTRY_DSN!,
     CUSTOMERIO_SITE_ID: process.env.CUSTOMERIO_SITE_ID!,

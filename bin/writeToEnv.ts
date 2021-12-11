dc86f29693b8846ed12e5c37c073163bcd2a1150
nate f <nmfurbearr@outlook.com>
2021-12-10 22:05:11 -0800

    testing gh actions

--- bin/writeToEnv.ts
+++ bin/writeToEnv.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const envFile = require("fs").createWriteStream(".env")
+const envFile = require("fs").createWriteStream(process.argv[3] || ".env");
 
-const dataFromParamStore = JSON.parse(process.argv[2])
+const dataFromParamStore = JSON.parse(process.argv[2]);
 
-const parameters = dataFromParamStore.Parameter.Value
+const parameters = dataFromParamStore.Parameter.Value;
 
-envFile.write(parameters)
+envFile.write(parameters);
